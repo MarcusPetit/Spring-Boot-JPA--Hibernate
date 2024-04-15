4a063fa2d5e9113268d1b101b964a38a6445704e
Marcus Petit <marcuspetit@hotmail.com>
2024-04-15 08:42:31 -0300

    User Serve com Mapping para id

---
+++ src/main/java/com/marcusprojeto/projetojpa/services/UserService.java
@@ -0,0 +1,24 @@
+package com.marcusprojeto.projetojpa.services;
+
+import com.marcusprojeto.projetojpa.entities.User;
+import com.marcusprojeto.projetojpa.repositories.UserRepositorie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepositorie repositorie;
+
+    public List<User> findeALl(){
+        return repositorie.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repositorie.findById(id);
+        return obj.get();
+    }
+}
