624beec5f4f7e32cc0ca255639adfbef0ad4d6d8
Marcus Petit <marcuspetit@hotmail.com>
2024-04-15 07:52:47 -0300

    User e UserResourcer

---
+++ src/main/java/com/marcusprojeto/projetojpa/entities/User.java
@@ -0,0 +1,76 @@
+package com.marcusprojeto.projetojpa.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String passowrd;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, String passowrd) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.passowrd = passowrd;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassowrd() {
+        return passowrd;
+    }
+
+    public void setPassowrd(String passowrd) {
+        this.passowrd = passowrd;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+}
