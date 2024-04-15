e8d553f92800cf11cd35b6e9f4bc95c81dae41b1
Marcus Petit <marcuspetit@hotmail.com>
2024-04-15 07:37:48 -0300

    Subindo projeto

---
+++ src/main/java/com/marcusprojeto/projetojpa/ProjetojpaApplication.java
@@ -0,0 +1,13 @@
+package com.marcusprojeto.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcusprojeto/projetojpa/ProjetojpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcusprojeto.projetojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
