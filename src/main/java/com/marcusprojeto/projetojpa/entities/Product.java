8fd2ce5bfbc37a9d73813cb0555f6f4baf3aba6f
Marcus Petit <marcuspetit@hotmail.com>
2024-04-17 05:19:47 -0300

    relacionar ManytoMany produto/categoria

--- src/main/java/com/marcusprojeto/projetojpa/entities/Product.java
+++ src/main/java/com/marcusprojeto/projetojpa/entities/Product.java
@@ -18,7 +18,7 @@ public class Product  implements Serializable {
     private Double price;
     private String imgUrl;
 
-
+    //para fazer relação muitos para muitos.
     @ManyToMany
     @JoinTable(name = "tb_product_category" , joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
