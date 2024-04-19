57ed536d8378d826c867e56d85764836c1fe7a09
Marcus Petit <marcuspetit@hotmail.com>
2024-04-19 16:51:08 -0300

    criando usuario metodo post

--- src/main/java/com/marcusprojeto/projetojpa/services/UserService.java
+++ src/main/java/com/marcusprojeto/projetojpa/services/UserService.java
@@ -21,4 +21,7 @@ public class UserService {
         Optional<User> obj = repositorie.findById(id);
         return obj.get();
     }
+    public User insert (User obj){
+        return repositorie.save(obj);
+    }
 }
